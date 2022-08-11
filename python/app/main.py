78c9b5fb8b3535d8816b52fa77e329f0b22eba03
Yadhunandan.Probakaran <Yadhunandan.Probakaran@solera.com>
2022-08-11 12:18:28 +0100

    move logger inside main()

--- python/app/main.py
+++ python/app/main.py
@@ -4,28 +4,29 @@ import logging
 import pathlib
 from zerossl import ZeroSSL
 
-# Setup logger
-LOG_FORMAT = '%(asctime)s %(levelname)s %(module)s : %(message)s'
-stdout_handler = logging.StreamHandler(sys.stdout)
-file_handler = logging.FileHandler("../log/application.log")
-handlers = [stdout_handler, file_handler]
-
-logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, handlers=handlers)
-logger = logging.getLogger(__name__)
 
 def main():
     """
     main() function
     """
+    # create log directory if not present
+    pathlib.Path('../log/'+ subdomain).mkdir(parents=True, exist_ok=True)
+    logger.info(f"Creating log/ directory to store logs")
+    
+    # Setup logger
+    LOG_FORMAT = '%(asctime)s %(levelname)s %(module)s : %(message)s'
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler("../log/application.log")
+    handlers = [stdout_handler, file_handler]
+
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, handlers=handlers)
+    logger = logging.getLogger(__name__)
 
     api_key = os.environ.get("ZEROSSL_API_KEY")
     subdomain = os.environ.get("SUBDOMAIN")
     aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
     aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-    # create log directory if not present
-    pathlib.Path('../log/'+ subdomain).mkdir(parents=True, exist_ok=True)
-    logger.info(f"Creating log/ directory to store logs")
 
     if api_key is None:
         logger.info("ZEROSSL_API_KEY environment variable is not set. Exiting...")
