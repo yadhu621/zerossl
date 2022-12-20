eb4c730ee68209bfef3df691d424ed0bdb5ceb73
Yadhu <yadhu621@gmail.com>
2022-12-20 12:26:01 +0000

    zerossl code changes

--- python/app/main.py
+++ python/app/main.py
@@ -1,7 +1,9 @@
+from ast import parse
 import os
 import sys
 import logging
 import pathlib
+import argparse
 from zerossl import ZeroSSL
 
 
@@ -22,7 +24,6 @@ def main():
     logger = logging.getLogger(__name__)
 
     api_key = os.environ.get("ZEROSSL_API_KEY")
-    subdomain = os.environ.get("SUBDOMAIN")
     aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
     aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
@@ -31,10 +32,6 @@ def main():
         logger.info("ZEROSSL_API_KEY environment variable is not set. Exiting...")
         exit()
         
-    if subdomain is None:
-        logger.info("SUBDOMAIN environment variable is not set. Exiting...")
-        exit()
-
     if aws_access_key_id is None:
         logger.info("AWS_ACCESS_KEY_ID environment variable is not set. Exiting...")
         exit()
@@ -43,10 +40,23 @@ def main():
         logger.info("AWS_SECRET_ACCESS_KEY environment variable is not set. Exiting...")
         exit()
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d','--domain-name',required=True)
+    args = parser.parse_args()
+
+    subdomain = args.domain_name
+ 
+
     # create cert directory if not present
     pathlib.Path('../cert/'+ subdomain).mkdir(parents=True, exist_ok=True)
     logger.info(f"Creating cert/ and cert/{subdomain} directories to store certificates")
 
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d','--domain-name',required=True)
+    args = parser.parse_args()
+
+    subdomain = args.domain_name
     zerossl = ZeroSSL(api_key, subdomain)
     zerossl.generate_csr().request_certificate().request_validation().download()
 
